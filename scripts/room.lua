7e7e8ad24f9b91c6d179d5f272a0b0fc37585010
ty <60381867+BadPig03@users.noreply.github.com>
2024-06-12 00:14:02 +0800

    02w19d
    1. 修复了加载mod多时的卡顿问题
    2. 修复了某些道具不出现的bug

--- scripts/room.lua
+++ scripts/room.lua
@@ -1,18 +1,19 @@
 local Room = ty:DefineANewClass()
 
-function Room:PostNewRoom()
-    local room = ty.GAME:GetRoom()
-    for i = 1, ty.ITEMCONFIG:GetCollectibles().Size - 1 do
-        if ItemConfig.Config.IsValidCollectible(i) and ty.ITEMCONFIG:GetCollectible(i):HasCustomTag("nolazarus") then
-            ty.ITEMPOOL:AddRoomBlacklist(i)
-            ty.ITEMPOOL:RemoveCollectible(i)
+function Room:PostNewLevel()
+    if PlayerManager.AnyoneIsPlayerType(PlayerType.PLAYER_LAZARUS_B) or PlayerManager.AnyoneIsPlayerType(PlayerType.PLAYER_LAZARUS2_B) then
+        for i = 1, ty.ITEMCONFIG:GetCollectibles().Size - 1 do
+            if ItemConfig.Config.IsValidCollectible(i) and ty.ITEMCONFIG:GetCollectible(i):HasCustomTag("nolazarus") and ty.ITEMPOOL:HasCollectible(i) then
+                ty.ITEMPOOL:RemoveCollectible(i)
+            end
         end
     end
 end
-Room:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, Room.PostNewRoom)
+Room:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, Room.PostNewLevel)
 
 function Room:PreAddCollectible(id, charge, firstTime, slot, varData, player)
-    if ItemConfig.Config.IsValidCollectible(id) and ty.ITEMCONFIG:GetCollectible(id):HasCustomTag("nolazarus") and (player:GetPlayerType() == PlayerType.PLAYER_LAZARUS_B or player:GetPlayerType() == PlayerType.PLAYER_LAZARUS2_B) then
+    local playerType = player:GetPlayerType()
+    if ItemConfig.Config.IsValidCollectible(id) and ty.ITEMCONFIG:GetCollectible(id):HasCustomTag("nolazarus") and (playerType == PlayerType.PLAYER_LAZARUS_B or playerType == PlayerType.PLAYER_LAZARUS2_B) then
         return CollectibleType.COLLECTIBLE_BREAKFAST
     end
 end
